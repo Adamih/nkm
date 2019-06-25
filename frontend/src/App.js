728552006148f9a36ece64b3d9418bf319a5af5f
Adam Henriksson <adam.ingemar.henriksson@gmail.com>
2019-06-25 17:01:01 +0200

    hooks up to solo api

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -11,24 +11,41 @@ import pages from './Pages-enum'
 import KM from "./content/KM";
 import Contact from "./content/Contact";
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <img src={bg} id={"bg"} alt={"Background"}/>
-                <Navbar onSelect={(id) => this.setState({page: id})}/>
+function App() {
+    const [loadingUsers, setLoadingUsers] = useState(false);
+    const [users, setUsers] = useState([])
+    
+    useEffect(() => {
+        const fetchUsers = async () => {
+            try {
+                setLoadingUsers(true)
+                const res = await axios.get("http://localhost:8000/api/account/users/");
+                setUsers(res.data);
+                setLoadingUsers(false)
+            } catch (e) {
+                console.log("error:", e)
+            }
+        }
 
-                <Switch>
-                    <Route exact path={"/"+pages.HOME} component={Home}/>
-                    <Route exact path={"/"+pages.SCHEDULE} component={Schedule}/>
-                    <Route exact path={"/"+pages.KM} component={KM}/>
-                    <Route exact path={"/"+pages.LOGIN} component={Login}/>
-                    <Route exact path={"/"+pages.CONTACT} component={Contact}/>}
-                    <Route component={Home}/>
-                </Switch>
-            </div>
-        )
-    }
+        fetchUsers();
+    }, []);
+
+    return (
+        <div className="App">
+            <img src={bg} id={"bg"} alt={"Background"}/>
+            <Navbar onSelect={(id) => this.setState({page: id})}/>
+
+            <Switch>
+                <Route exact path={"/"+pages.HOME} component={Home}/>
+                <Route exact path={"/"+pages.SCHEDULE} component={Schedule}/>
+                <Route exact path={"/"+pages.KM} component={KM} loadingUsers={loadingUsers}/>
+                <Route exact path={"/"+pages.LOGIN} component={Login}/>
+                <Route exact path={"/"+pages.CONTACT} component={Contact}/>}
+                <Route component={Home}/>
+            </Switch>
+        </div>
+    )
+
 }
 
 export default App;
--- frontend/src/content/KM.js
+++ frontend/src/content/KM.js
@@ -2,7 +2,6 @@ import React, {Component} from 'react';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import logo from '../images/logo.png';
 import '../css/KM.css'
 import Image from "react-bootstrap/Image";
 import ReactCardFlip from "react-card-flip";
@@ -20,11 +19,9 @@ class KM extends Component {
     async componentDidMount() {
         this.setState({isLoading: true});
         try {
-            const res = await axios.get("http://172.17.0.1:5000/api/user");
-            const json = res.data;
-            const data = json.data;
-            console.log("data:",data);
-            this.setState({data: data, isLoading: false});
+            const res = await axios.get("http://localhost:8000/api/account/users/");
+            console.log("data:",res.data);
+            this.setState({data: res.data, isLoading: false});
         } catch (e) {
             console.log("error:", e)
         }
@@ -39,7 +36,13 @@ class KM extends Component {
         let content = [];
         const users = this.state.data;
         users.map((user) => {
-            content.push(<PresentationCard _name={user.full_name} _title={user.title} />);
+            if (user.profile != null) {
+                content.push(
+                    <PresentationCard _name={user.profile.name} 
+                                      _title={user.profile.title}
+                                      _pooper={user.profile.proper}
+                                      _party={user.profile.party} />);
+            }
         });
 
         content = content.map((card, i) => {
@@ -69,15 +72,14 @@ class PresentationCard extends Component {
     }
 
     render(){
-        const { _name, _title } = this.props;
         return (
             <div className={"person-card-wrapper"} onClick={() => this.setState ({ isFlipped: !this.state.isFlipped })}>
                 <ReactCardFlip isFlipped={this.state.isFlipped} flipDirection="horizontal">
-                    <Image className={"portrait"} src={logo} key={"front"} />
-                    <Image className={"portrait"} src={logo} key={"back"}/>
+                    <Image className={"portrait"} src={this.props._pooper} key={"front"} />
+                    <Image className={"portrait"} src={this.props._party} key={"back"}/>
                 </ReactCardFlip>
-                <h1>{_name}</h1>
-                <h3>{_title}</h3>
+                <h1>{this.props._name}</h1>
+                <h3>{this.props._title}</h3>
             </div>
         )
     }
